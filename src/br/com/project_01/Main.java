a692e3ec6e5fa802202697a7ef1414653e85d44a
Buzzola <maycon.buzzola@gmail.com>
2022-01-14 01:16:51 -0300

    Basics

--- src/br/com/project_01/Main.java
+++ src/br/com/project_01/Main.java
@@ -1,4 +1,5 @@
 package br.com.project_01;
+
 import java.util.Locale;
 import java.util.Scanner;
 import br.com.utils.Calculator;
@@ -6,54 +7,44 @@ import br.com.utils.Calculator;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("Project_01");
 		System.out.println("Esse programa verifica qual bloco teve um valor maior na soma dos 3 numeros digitados");
 		System.out.println("-------------------------------------------------------");
-		
-		
-		
-		//Pega os dados do P1.
+
+		// Pega os dados do P1.
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		Calculator calculator = new Calculator();
-		
+
 		System.out.println("Digite o primeiro numero do bloco 1");
 		calculator.p1a = sc.nextDouble();
-		
+
 		System.out.println("Digite o segundo numero do bloco 1");
 		calculator.p1b = sc.nextDouble();
-		
+
 		System.out.println("Digite o terceiro numero do bloco 1");
 		calculator.p1c = sc.nextDouble();
-		
-		System.out.println("Agora insira os dados do bloco 2");
-		
-		//Pega os dados do P2.
-		calculator.p2a = sc.nextDouble();
+
+		System.out.println("BLOCO 1 FINALIZADO, AGORA DADOS DO BLOCO 2");
+		System.out.println("-----------------------------------------------------------");
+
+		// Pega os dados do P2.
 		System.out.println("Digite o primeiro numero do bloco 2");
-		
-		calculator.p2b = sc.nextDouble();
+		calculator.p2a = sc.nextDouble();
+
 		System.out.println("Digite o segundo numero do bloco 2");
-		
-		calculator.p2c = sc.nextDouble();
+		calculator.p2b = sc.nextDouble();
+
 		System.out.println("Digite o terceiro numero do bloco 2");
-		sc.close();
-		
-		
-		//Printa qual bloco teve a soma maior
+		calculator.p2c = sc.nextDouble();
+
+		// Printa qual bloco teve o maior resultado da soma.
 		calculator.howBigger();
-		System.out.println("BLOCO 1: "+ calculator.calculatorP1());
-		System.out.println("BLOCO 2: "+ calculator.calculatorP2());
+		System.out.println("BLOCO 1: " + calculator.calculatorP1());
+		System.out.println("BLOCO 2: " + calculator.calculatorP2());
 
-		
-		
-		
-		
-		
-		
-		
-
+		sc.close();
 	}
 
 }
--- src/br/com/utils/Calculator.java
+++ src/br/com/utils/Calculator.java
@@ -1,45 +1,37 @@
 package br.com.utils;
 
 public class Calculator {
-	
-	
-	//p1. = bloco 1
-	public double p1a, p1b, p1c;
-	public double p1all = p1a + p1b + p1c;
-	//p2. = bloco 2
-	public double p2a, p2b, p2c;
-	public double p2all = p2a + p2b + p2c;
 
-	
-	//Calcula o valor do bloco p1.
+	// p1. = bloco 1
+	public double p1a, p1b, p1c;
+
+	// p2. = bloco 2
+	public double p2a, p2b, p2c;
+
+	// Calcula o valor do bloco p1.
 	public double calculatorP1() {
-		
 		return p1a + p1b + p1c;
 	}
-	
-	//Calcula o valor do bloco p2.
+
+	// Calcula o valor do bloco p2.
 	public double calculatorP2() {
-		
 		return p2a + p2b + p2c;
 	}
 
-	
-	//Calcula qual bloco teve o maior valor.
+	// Calcula qual bloco teve o maior valor.
 	public void howBigger() {
-		
-		
+
 		if (calculatorP1() > calculatorP2()) {
 			System.out.println("O primeiro bloco teve um numero maior na soma total.");
-			
+
 		} else if (calculatorP1() == calculatorP2()) {
 			System.out.println("O bloco 1 e 2 teve o mesmo valor");
-			
+
 		} else if (calculatorP1() < calculatorP2()) {
 			System.out.println("O segundo bloco teve um numero maior na soma total");
-			
+
 		}
-		
+
 	}
-	
-	
+
 } // final
